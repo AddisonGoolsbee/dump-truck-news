0243bf01cb23ff82bf92f88debc84fe6e92ef767
Addison Goolsbee <addisongoolsbee@gmail.com>
2025-12-13 16:36:46 -0800

    Fix mobile header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <div className="bg-white border-b border-neutral-200 font-serif">
-      <div className="relative mx-auto px-5 pt-9 pb-8 md:px-10">
+      <div className="relative mx-auto pt-9 pb-8 md:px-10">
         <Link
           to="/about"
           className="absolute right-4 sm:right-8 top-4 sm:top-5 text-xs sm:text-base font-medium uppercase tracking-wider text-neutral-500 transition-colors duration-150 hover:text-neutral-700"
